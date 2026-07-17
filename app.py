6dc5c4541c214141689704883f6aa79b24a60c99
Ankit Kumar <ak8622564@gmail.com>
2026-07-17 22:10:58 +0530

    modified changes

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def search():
 
 
 if __name__ == '__main__':
-    app.run(debug=os.environ.get('FLASK_DEBUG') == '1', port=5000)
+    app.run(debug=os.environ.get('FLASK_DEBUG') == '1', port=8000)
